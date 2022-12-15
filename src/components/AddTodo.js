24d8b1605cf76ba23af1ddb35d93553c36ef1f70
ajay kumar  shah <110763605+ajaykumarshah@users.noreply.github.com>
2022-12-15 14:16:33 +0530

    k

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 
 const AddTodo = ({ dispatch }) => {
 
-    const [taskArr, setTaskArr] = useState([]);
-    const [inputVal, setVal] = useState("");
-    const [DateId, setId] = useState(new Date().getTime().toString());
+    const [taskArr,setTaskArr] =useState([]);
+    const [inputVal,setVal] = useState("");
+    const [DateId,setId] = useState( new Date().getTime().toString());
 
-    function submitHandler(e) {
-        e.preventDefault();
-        setId(new Date().getTime().toString());
+    function submitHandler (e) {
+        e.preventDefault();  
+        setId( new Date().getTime().toString()) ;
         setTaskArr(
             [...taskArr,
-            { id: DateId, title: inputVal }
+                {id:DateId,title:inputVal}
             ]
         )
-        dispatch({ type: "ADD", payload: { title: inputVal, id: DateId } });
+        dispatch({type:"ADD",payload:{title:inputVal,id:DateId}});
         setVal("");
     }
 
@@ -25,10 +25,10 @@ const AddTodo = ({ dispatch }) => {
 
     return (
         <>
-            <form id="todo-form" onSubmit={submitHandler}>
-                <input id="todo-input" type="textarea" required onChange={inputFun} value={inputVal} />
-                <button type="submit">submit</button>
-            </form>
+        <form id="todo-form" onSubmit={submitHandler}>
+            <input id="todo-input" type="textarea" required onChange={inputFun} value={inputVal}/>
+            <button type="submit">submit</button>
+        </form>
         </>
     )
 }
--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,14 @@ import '../styles/App.css';
 import { AddTodo } from './AddTodo';
 import { Todo } from './Todo';
 const App = () => {
-  const [state, dispatch] = useReducer(todoReducer, [])
-
-
+  const [state,dispatch] = useReducer(todoReducer,[])
+  
+ 
   return (
     <div id="main">
       <AddTodo dispatch={dispatch} state={state} />
-      <Todo dispatch={dispatch} state={state} />
-      {/* Render list of Todo Components here */}
+      <Todo dispatch={dispatch} state={state}/>
+     {/* Render list of Todo Components here */}
     </div>
   )
 }
